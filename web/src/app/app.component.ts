9f9dfc711e36fa001a38ab79d7a8fe5912c654df
Ana-beatriz2 <caldas.ana9@gmail.com>
2025-01-13 19:22:26 -0300

    feat: inicia frontend

---
+++ web/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { OrdersComponent } from './components/orders/orders.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, OrdersComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'web';
+}
