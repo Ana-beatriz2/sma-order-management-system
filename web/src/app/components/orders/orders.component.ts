1c6bf1b73b320a2f8e2aad731b90f906ac737f87
Ana-beatriz2 <caldas.ana9@gmail.com>
2025-01-22 21:51:03 -0300

    feat: adiciona socket.io no componente da tela dos pedidos

--- web/src/app/components/orders/orders.component.ts
+++ web/src/app/components/orders/orders.component.ts
@@ -7,6 +7,7 @@ import { SocketService } from '../../services/socket.service';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { OrdersChangedService } from '../../services/orders-changed.service';
 
 @Component({
   selector: 'app-orders',
@@ -17,20 +18,23 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class OrdersComponent implements OnInit, OnDestroy {
   orders: any[] = [];
+  changedOrdersId: any[] = [];
   private ordersTopic = 'receiveOrders';
   private subscription!: Subscription;
 
-  constructor(private socketService: SocketService, private ngZone: NgZone) {
-
+  constructor(
+    private socketService: SocketService,
+    private ngZone: NgZone,
+    private ordersChangedService: OrdersChangedService
+  ) {
   }
 
   ngOnInit(): void {
-      this.subscription = this.socketService.on(this.ordersTopic).subscribe((data) => {
-        console.log('New order received:', data);
-        this.ngZone.run(() => {
-          this.orders = data;
-        });
+    this.subscription = this.socketService.on(this.ordersTopic).subscribe((data) => {
+      this.ngZone.run(() => {
+        this.orders = data;
       });
+    });
   }
 
   ngOnDestroy(): void {
@@ -48,5 +52,7 @@ export class OrdersComponent implements OnInit, OnDestroy {
       const [finishedOrder] = this.orders.splice(orderToChangeStatusindex, 1);
       this.orders.push(finishedOrder);
     }
+
+    this.ordersChangedService.sendOrderWithChangedStatus(order);
   }
 }
